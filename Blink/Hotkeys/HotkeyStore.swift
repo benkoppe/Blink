249177fe5fc4c2c3ad6896122d874d36cdc4c161
ben <koppe.development@gmail.com>
2026-03-24 02:40:10 -0400

    Feat: basic hotkey setup

---
+++ Blink/Hotkeys/HotkeyStore.swift
@@ -0,0 +1,16 @@
+//
+//  HotkeyStore.swift
+//  Blink
+//
+//  Created by Ben on 3/24/26.
+//
+
+import AppKit
+import Carbon
+import Foundation
+import Observation
+
+@Observable
+final class HotkeyStore {
+    // MARK: - Stored hotkeys
+}
