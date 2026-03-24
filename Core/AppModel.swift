2f51edb111c47724503fa882ad416ebc7093b486
ben <koppe.development@gmail.com>
2026-03-24 00:40:38 -0400

    Create simple AppModel class

---
+++ Core/AppModel.swift
@@ -0,0 +1,14 @@
+//
+//  AppModel.swift
+//  Blink
+//
+//  Created by Ben on 3/24/26.
+//
+
+import Foundation
+import Observation
+
+@Observable
+final class AppModel {
+    let spaceSwitcher = SpaceSwitcher()
+}
