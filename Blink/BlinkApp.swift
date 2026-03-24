f5ccecfa7c07a5888012c29d141ac11562898aed
ben <koppe.development@gmail.com>
2026-03-23 23:56:39 -0400

    Initial Commit

---
+++ Blink/BlinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlinkApp.swift
+//  Blink
+//
+//  Created by Ben on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct BlinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
