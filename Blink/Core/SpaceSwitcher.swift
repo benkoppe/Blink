a178bc08f7847d8ed588f035630230ab98675e83
ben <koppe.development@gmail.com>
2026-04-08 16:02:19 -0400

    Remove call to debugLogSpaceInfo

--- Blink/Core/SpaceSwitcher.swift
+++ Blink/Core/SpaceSwitcher.swift
@@ -193,7 +193,7 @@ final class SpaceSwitcher {
     func refreshSpaceInfo() {
         // Use the menu-bar display for the icon (always correct on multi-monitor)
         spaceInfo = loadSpaceInfo(useCursorDisplay: false)
-        debugLogSpaceInfo()
+        // debugLogSpaceInfo()
     }
 
     // MARK - Workspace notifications
